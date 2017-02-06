3e8acba7237333013ff38f4c3c453b1b8abab605
Rafał Miler <rafalmiler77@gmail.com>
2017-02-06 15:16:06 +0100

    background layer1 pic added and cleancoded

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import { Grid, Row, Col, Button } from 'react-bootstrap'
 
 import './App.css';
 
-let img1= process.env.PUBLIC_URL
-let img2= '/images/layer1.jpeg'
-
 class App extends Component {
   render() {
 
     return (
 
-
-        <Grid fluid style={{backgroundImage: 'url("' + img1 + img2 + '")', backgroundSize: 'cover', minHeight: '800px'}}>
-          <Row   className="show-grid">
-            <Col  xs={6} md={4}>
+        <Grid fluid style={{backgroundImage: 'url("' + process.env.PUBLIC_URL + '/images/layer1.jpeg")', backgroundSize: 'cover', minHeight: '800px'}}>
+          <Row className="show-grid">
+            <Col xs={6} md={4}>
               <Button bsStyle="success">Get started today</Button>
             </Col>
           </Row>
-          {/*<img src={img1 + img2}*/}
-          />
         </Grid>
 
     );
@@ -27,6 +21,3 @@ class App extends Component {
 }
 
 export default App;
-{/*<img src={process.env.PUBLIC_URL + '/images/layer1.jpeg'}*/}
-     {/*role="presentation"*/}
-{/*/>*/}
