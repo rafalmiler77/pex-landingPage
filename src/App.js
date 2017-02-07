739988fc58f977c38f1ffce77fb86f699b4f1c05
Rafał Miler <rafalmiler77@gmail.com>
2017-02-07 11:35:53 +0100

    layer one done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import './App.css';
 class App extends Component {
   render() {
 
+      let btnCustomStyle={
+        "backgroundImage" : "none",
+        "backgroundColor" : "#8600cb"
+      };
     return (
 
-        <Grid fluid style={{backgroundImage: 'url("' + process.env.PUBLIC_URL + '/images/layer1.jpeg")', backgroundSize: 'cover', minHeight: '800px'}}>
-          <Row className="show-grid">
+        <Grid fluid>
+          <Row className="show-grid" style={{background: 'url("' + process.env.PUBLIC_URL + '/images/layer1.jpeg") no-repeat center center fixed', backgroundSize: 'cover', minHeight: '800px'}}>
             <Col xs={6} md={4}>
-              <Button bsStyle="success">Get started today</Button>
+              <h2 className="title-layer1">Designs your eyeballs</h2>
+              <h2 className="title-layer1">will thank you for</h2>
+              <Button style={btnCustomStyle} bsStyle="primary" className='button-layer1' >Get started today</Button>
+              <p>lkdjfkj fsj sdf lsad fls fll flsf sl flskf  rfflsa jf kls</p>
             </Col>
           </Row>
         </Grid>
-
     );
   }
 }
