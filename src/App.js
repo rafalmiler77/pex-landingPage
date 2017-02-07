03722640b267eaae776ee11088e8ccf516165cb6
Rafał Miler <rafalmiler77@gmail.com>
2017-02-07 11:46:43 +0100

    text in columns added after layer1

--- src/App.js
+++ src/App.js
@@ -13,12 +13,26 @@ class App extends Component {
     return (
 
         <Grid fluid>
-          <Row className="show-grid" style={{background: 'url("' + process.env.PUBLIC_URL + '/images/layer1.jpeg") no-repeat center center fixed', backgroundSize: 'cover', minHeight: '800px'}}>
-            <Col xs={6} md={4}>
+          <Row className="show-grid" style={{background: 'url("' + process.env.PUBLIC_URL + '/images/layer1.jpeg") no-repeat center center', backgroundSize: 'cover', minHeight: '800px'}}>
+            <Col xs={6}>
               <h2 className="title-layer1">Designs your eyeballs</h2>
               <h2 className="title-layer1">will thank you for</h2>
               <Button style={btnCustomStyle} bsStyle="primary" className='button-layer1' >Get started today</Button>
               <p>lkdjfkj fsj sdf lsad fls fll flsf sl flskf  rfflsa jf kls</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={3} xsOffset={3}>
+              <h4>A fantastic heading</h4>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+              <h4>Blaz robar for president</h4>
+              <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </Col>
+            <Col xs={3}>
+              <h4>Good day blokes</h4>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+              <h4>Vote for Robar</h4>
+              <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </Col>
           </Row>
         </Grid>
