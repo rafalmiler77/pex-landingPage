3e4ca06066c67bd4c340de16ac6f52b5d3bf806e
Rafał Miler <rafalmiler77@gmail.com>
2017-02-07 21:07:32 +0100

    violet rectangular positioned

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ class App extends Component {
           backgroundSize: 'cover',
           minHeight: '800px'
         }}>
-          <Col xs={6}>
+          <Col className='firstLayer' xs={6}>
             <h2 className="title-layer1">Designs your eyeballs</h2>
             <h2 className="title-layer1">will thank you for</h2>
             <Button style={btnCustomStyle} bsStyle="primary" className='button-layer1'>Get started today</Button>
-            <p>lkdjfkj fsj sdf lsad fls fll flsf sl flskf rfflsa jf kls</p>
+            <p className="textUnderButton" >lkdjfkj fsj sdf lsad fls fll flsf sl flskf rfflsa jf kls</p>
           </Col>
         </Row>
         <Row className="rectAfterLayer1">
@@ -61,11 +61,14 @@ class App extends Component {
             <p>lkdjfkj fsj sdf lsad fls fll flsf sl flskf rfflsa jf kls</p>
           </Col>
           <Row className="rectInLayer2">
-            <Col xs={6} xsOffset={3}>
+            <Col xs={10} xsOffset={1} sm={6} smOffset={3} md={4} mdOffset={4} lg={4} lgOffset={4}>
               <h3>Experience the rush today</h3>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                 industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
                 scrambled</p>
+              <Button style={btnCustomStyle} bsStyle="primary" className='button-layer2'>Get started today</Button>
+              <p className="textUnderButton2" >lkdjfkj fsj sdf lsad fls fll flsf sl flskf rfflsa jf kls</p>
+
             </Col>
           </Row>
         </Row>
