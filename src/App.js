a186aaaa53e9d289f39f1bfd67086c7b2dcdc783
Rafał Miler <rafalmiler77@gmail.com>
2017-02-07 21:31:47 +0100

    text in whiteLayer added

--- src/App.js
+++ src/App.js
@@ -73,7 +73,16 @@ class App extends Component {
           </Row>
         </Row>
         <Row className="whiteLayer">
-
+          <Col xs={8} xsOffset={1} sm={6} smOffset={1}  md={4} mdOffset={2} lg={4} lgOffset={2}>
+          <h4>A heading is what you need here</h4>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled</p>
+          <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.</p>
+          </Col>
         </Row>
         <Row className="show-grid" style={{
           background: 'url("' + process.env.PUBLIC_URL + '/images/layer3.jpeg") no-repeat center center',
